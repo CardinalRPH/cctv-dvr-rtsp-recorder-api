6d24fb95ee7fe44ada463271eef6e3c3e0fafdfa
CardinalRPH <rayhanfebriyan62@gmail.com>
2025-11-28 02:51:19 +0000

    update userModel

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,7 +1,44 @@
+import { Prisma } from "../generated/prisma/client"
 import { prisma } from "../libs/prisma"
 
-const getUniqueUser = async (id: string) => {
-    await prisma.user.findUnique({
-        where: { id }
+export const getUniqueUser = async (condition:Prisma.UserWhereUniqueInput) => {
+    return await prisma.user.findUnique({
+        where: condition
+    })
+}
+
+export const getAllUser = async (limit = 50, cursor?: string, codition?: Prisma.UserWhereInput) => {
+    return await prisma.user.findMany({
+        cursor: {
+            id: cursor
+        },
+        take: limit,
+        where: codition,
+        omit: {
+            password: true
+        }
+    })
+}
+
+export const createUser = async (data: Prisma.UserCreateInput) => {
+    return await prisma.user.create({
+        data
+    })
+}
+
+export const updateUser = async (data: Prisma.UserUpdateInput, id: string) => {
+    return await prisma.user.update({
+        data,
+        where: {
+            id
+        }
+    })
+}
+
+export const deleteuser = async (id: string) => {
+    return await prisma.user.delete({
+        where: {
+            id
+        }
     })
 }
